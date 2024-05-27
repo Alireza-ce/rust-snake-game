45455fe6f4493f3314c49b46e9dd69c7d215addb
Alireza <alireza@Alirezas-MacBook-Pro.local>
2024-05-27 11:53:02 +0330

    feat: add global allocator to reduce wasm size

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 use wasm_bindgen::prelude::*;
 
+// this allocator will reduce the .wasm file size
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(name);
