a6c41f23e67fb0952efc31450d88c3433d4910b3
Alireza <alireza@Alirezas-MacBook-Pro.local>
2024-06-02 09:34:54 +0330

    feat: draw the world and snake

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,40 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+}
+
+impl Snake {
+    pub fn new(initial_index: usize) -> Snake {
+        Snake {
+            body: vec![SnakeCell(initial_index)],
+        }
+    }
+}
+
 #[wasm_bindgen]
-struct World {
-    pub width: i32,
+pub struct World {
+    width: i32,
+    snake: Snake,
 }
 
 #[wasm_bindgen]
 impl World {
     pub fn new() -> World {
-        World { width: 8 }
+        World {
+            width: 8,
+            snake: Snake::new(10),
+        }
     }
 
     pub fn width(&self) -> i32 {
         self.width
+    }
+
+    pub fn snake_head(&self) -> usize {
+        self.snake.body[0].0
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,6 +1,50 @@
 import init, { World } from 'snake_game';
 
-init().then(res => {
-    const world = World.new();
-    console.log(world.width());
-})
+
+
+function drawWorld(width, cellSize, snakeIdx){
+    const canvas = document.getElementById('board-game-canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.height = width * cellSize;
+    canvas.width = width * cellSize;
+
+    ctx.beginPath();
+
+    for (let x = 0 ; x < width + 1; x++){
+        ctx.moveTo(cellSize * x,0);
+        ctx.lineTo(cellSize * x , width  * cellSize)
+    }
+
+    for (let y = 0 ; y < width + 1; y++){
+       ctx.moveTo( 0,cellSize * y);
+       ctx.lineTo(width  * cellSize ,cellSize * y )
+    }
+
+    ctx.stroke();
+
+    const col = snakeIdx % width;
+    const row = Math.floor(snakeIdx / width);
+
+    ctx.beginPath();
+    ctx.fillRect(
+        col * cellSize,
+        row * cellSize,
+        cellSize,
+        cellSize
+    )
+
+    ctx.stroke();
+}
+
+
+function main(){
+    const cellSize = 20;
+    init().then(res => {
+        const world = World.new();
+        console.log(world.width());
+        drawWorld(world.width(),cellSize, world.snake_head());
+    })
+}
+
+
+main();
